a4006ebd4f1a12d77b13537fe1eadc4679ecc7cb
Emanuel Schweizer <emanuel.schweizer@icloud.com>
2026-08-14 14:13:24 +0200

    SharedListService completed

--- ShoppingList_WebAPI/Services/SharedLists/SharedListService.cs
+++ ShoppingList_WebAPI/Services/SharedLists/SharedListService.cs
@@ -1,17 +1,50 @@
+using Microsoft.EntityFrameworkCore;
 using ShoppingList_WebAPI.Data;
 using ShoppingList_WebAPI.DTOs.SharedListDTOs;
+using ShoppingList_WebAPI.Models;
 
 namespace ShoppingList_WebAPI.Services.SharedLists;
 
 public class SharedListService(AppDbContext context) : ISharedListService
 {
-    public Task ShareListAsync(int ownerId, int listId, SharedListRequest req, CancellationToken ct)
+    public async Task ShareListAsync(int ownerId, int listId, SharedListRequest req, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var listExist = await context.ShoppingLists
+            .AnyAsync(x => x.Id == listId && x.OwnerId == ownerId, ct);
+        if (!listExist)
+            throw new KeyNotFoundException("List not found");
+        
+        var sharedUser = await context.Users.FirstOrDefaultAsync(x => x.Email == req.Email, ct);
+        if (sharedUser == null)
+            throw new KeyNotFoundException("Email not found");
+        
+        if (sharedUser.Id == ownerId)
+            throw new InvalidOperationException("You cannot share a list with yourself");
+        
+        var recordExist = await context.SharedLists.AnyAsync(x => x.ListId == listId && x.UserId == sharedUser.Id, ct);
+        if (recordExist)
+            throw new InvalidOperationException("List is already shared with this user");
+
+        var newRecord = new SharedList
+        {
+            UserId = sharedUser.Id,
+            ListId = listId
+        };
+
+        context.SharedLists.Add(newRecord);
+        await context.SaveChangesAsync(ct);
     }
 
-    public Task UnShareListAsync(int ownerId, int listId, int sharedUserId, CancellationToken ct)
+    public async Task UnShareListAsync(int ownerId, int listId, int sharedUserId, CancellationToken ct)
     {
-        throw new NotImplementedException();
+        var record = await context.SharedLists
+            .FirstOrDefaultAsync(x => 
+                x.ListId == listId && x.UserId == sharedUserId && x.ShoppingList.OwnerId == ownerId, ct);
+        
+        if (record == null)
+            throw new KeyNotFoundException("Shared record not found");
+        
+        context.Remove(record);
+        await context.SaveChangesAsync(ct);
     }
 }
