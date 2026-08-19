bd1ad54ebc1994630276bbdeaa4dd1b730c8b2cb
Emanuel Schweizer <emanuel.schweizer@icloud.com>
2026-08-19 07:08:52 +0200

    db migration: AddSharedListUniqueIndex

---
+++ ShoppingList_WebAPI/Migrations/20260819050743_AddSharedListUniqueIndex.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace ShoppingList_WebAPI.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddSharedListUniqueIndex : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SharedLists_ListId",
+                table: "SharedLists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedLists_ListId_UserId",
+                table: "SharedLists",
+                columns: new[] { "ListId", "UserId" },
+                unique: true);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_SharedLists_ListId_UserId",
+                table: "SharedLists");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_SharedLists_ListId",
+                table: "SharedLists",
+                column: "ListId");
+        }
+    }
+}
